91b56c5cac48625adc8daab0c483bb96af03314f
Mohit Yadav <39617209+mohit810@users.noreply.github.com>
2021-08-01 16:15:29 +0530

    Add files via upload

---
+++ masterdb_ambulance_table.sql
@@ -0,0 +1,71 @@
+-- MySQL dump 10.13  Distrib 8.0.24, for macos11 (x86_64)
+--
+-- Host: covi-helper-db.cv0tu95xmuks.ap-south-1.rds.amazonaws.com    Database: masterdb
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
+
+--
+-- Table structure for table `ambulance_table`
+--
+
+DROP TABLE IF EXISTS `ambulance_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ambulance_table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `website` varchar(100) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lead_type` varchar(255) NOT NULL,
+  `statecode` int(11) NOT NULL,
+  `citycode` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `map_link` varchar(255) NOT NULL,
+  `phone_number` varchar(45) NOT NULL,
+  `whatsapp_number` varchar(45) NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  `creation_time` varchar(100) NOT NULL,
+  `verified_by` varchar(100) NOT NULL,
+  `verified_time` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ambulance_table`
+--
+
+LOCK TABLES `ambulance_table` WRITE;
+/*!40000 ALTER TABLE `ambulance_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ambulance_table` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-01 16:12:19
